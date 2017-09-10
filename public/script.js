8260b0f345e59bc651897e5dd0eb6bf7afee81a3
Andrea Selinger <Seli@MacBook-Pro-2.local>
2017-09-10 16:51:44 +0300

    Finished basic tasks

--- public/script.js
+++ public/script.js
@@ -1,35 +1,67 @@
 // an array with all of our cart items
-var cart = [];
 
-var updateCart = function () {
-  // TODO: Write this function. In this function we render the page.
-  // Meaning we make sure that all our cart items are displayed in the browser.
-  // Remember to empty the "cart div" before you re-add all the item elements.
+
+var ShoppingApp = function () {
+  var _cart = [];
+
+  var _updateCart = function () {
+    // TODO: Write this function. In this function we render the page.
+    // Meaning we make sure that all our cart items are displayed in the browser.
+    // Remember to empty the "cart div" before you re-add all the item elements.
+    var total = 0;
+    var $shoppingCart = $(".cart-list");
+    $shoppingCart.empty();
+    
+    var source = $('#shoppingCart-template').html();
+    var template = Handlebars.compile(source);
+
+    for (var i = 0; i < _cart.length; i++) {
+      total += _cart[i].price;
+      var newHTML = template(_cart[i]);
+      
+      $shoppingCart.append(newHTML);
+      //shoppingCart.append("<div>" + cartItem.name + " - " + cartItem.price + "</div>");
+    }
+    $('.total').html(total);
+  }
+
+
+  var addItem = function (item) {
+    // TODO: Write this function. Remember this function has nothing to do with sdisplay. 
+    // It simply is for adding an item to the cart array, no HTML involved - honest ;-)
+    _cart.push(item);
+    _updateCart();
+  }
+
+  var clearCart = function () {
+    // TODO: Write a function that clears the cart ;-)
+    _cart = [];
+    _updateCart();
+
+  }
+
+  return {
+    addItem: addItem,
+    clearCart: clearCart
+  };
+
 }
 
-
-var addItem = function (item) {
-  // TODO: Write this function. Remember this function has nothing to do with display. 
-  // It simply is for adding an item to the cart array, no HTML involved - honest ;-)
-}
-
-var clearCart = function () {
-  // TODO: Write a function that clears the cart ;-)
-}
+var app = ShoppingApp();
 
 $('.view-cart').on('click', function () {
-  // TODO: hide/show the shopping cart!
+  $('.shopping-cart').toggleClass('show');
 });
 
 $('.add-to-cart').on('click', function () {
-  // TODO: get the "item" object from the page
-  addItem(item);
-  updateCart();
+  var item = $(this).closest('.item').data();
+  app.addItem(item);
 });
 
 $('.clear-cart').on('click', function () {
-  clearCart();
+  app.clearCart();
 });
 
-// update the cart as soon as the page loads!
-updateCart();
+
+
+
